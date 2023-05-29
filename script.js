8337b13f4cdbf351dfcfa0f382dcbf27fc23d8f0
Paloma Barboza <132226664+pbarbo@users.noreply.github.com>
2023-05-28 23:12:07 -0300

    Add files ChallengeEncriptador

---
+++ script.js
@@ -0,0 +1,69 @@
+const textArea = document.querySelector(".texto");
+const mensaje = document.querySelector(".resultado");
+
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+//se va a trabajar en arreglos multidimensionales 
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada
+}
+
+
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada
+}
+
+function btnCopiar(){
+    copiar();
+}
+
+function copiar(){
+    let resultado = document.querySelector(".resultado");
+    let boton = document.querySelector(".copiar");
+    navigator.clipboard.writeText(resultado.value);
+    boton.textContent = "copiado";
+    resultado.value = "";
+
+    setTimeout(function(){
+        boton.textContent = "copiar";     
+    }, 3000);
+}
+
+
